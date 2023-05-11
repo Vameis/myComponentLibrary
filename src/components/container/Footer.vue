d120108a693db19d0a4e40c5a2dd0b0a4580bcf3
Vameis <88279644+Vameis@users.noreply.github.com>
2023-05-11 22:02:59 +0800

    feat: 添加container组件

---
+++ src/components/container/Footer.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer class="el-footer" :style="{ height }">
+        <slot />
+    </footer>
+</template>
+  
+<script lang="ts">
+export default {
+    name: 'ElFooter'
+}
+</script>
+  
+<script setup lang="ts">
+import { withDefaults } from 'vue'
+
+withDefaults(
+    defineProps<{
+        height?: string
+    }>(), {
+    height: "60px"
+}
+)
+
+</script>
+  
+<style lang="scss">
+@import '@/styles/mixin.scss';
+
+@include b(footer) {
+    padding: $--footer-padding;
+    box-sizing: border-box;
+    flex-shrink: 0;
+}
+</style>
---
+++ src/components/container/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="el-header" :style="{ height }">
+        <slot />
+    </header>
+</template>
+  
+<script lang="ts">
+export default {
+    name: 'ElHeader'
+}
+</script>
+<script setup lang="ts">
+import { withDefaults } from 'vue'
+
+withDefaults(
+    defineProps<{
+        height?: string
+    }>(), {
+    height: "60px"
+})
+
+</script>
+  
+<style lang="scss">
+@import '@/styles/mixin.scss';
+
+@include b(header) {
+    padding: $--header-padding;
+    box-sizing: border-box;
+    flex-shrink: 0;
+}
+</style>
