42cac3bbaedb5b1999c589d5a25872efd2b0a53a
Vameis <88279644+Vameis@users.noreply.github.com>
2023-05-11 22:10:45 +0800

    feat: 注册container组件

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElContainer from './components/container/index'
 
-createApp(App).mount('#app')
+createApp(App).use(ElContainer).mount('#app')
