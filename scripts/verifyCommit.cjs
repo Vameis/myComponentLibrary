bc481cc6e278dda61a3d1b9f60bda9bad7621f60
Vameis <88279644+Vameis@users.noreply.github.com>
2023-05-11 20:07:06 +0800

    feat:  修改commit验证的正则

--- scripts/verifyCommit.cjs
+++ scripts/verifyCommit.cjs
@@ -9,7 +9,7 @@ if (!commitRE.test(msg)) {
   if(!mergeRe.test(msg)){
     console.log('git commit信息校验不通过')
 
-    console.error(`git commit的信息格式不对, 需要使用 title(scope): desc的格式
+    console.error(`git commit的信息格式不对, 需要使用 title: desc的格式
       比如 fix: xxbug
       feat(test): add new 
       具体校验逻辑看 scripts/verifyCommit.cjs
