6ebd78d9a156e8d264387f602c8a961132875b94
Vameis <88279644+Vameis@users.noreply.github.com>
2023-05-13 18:39:58 +0800

    feat: 新增验证信息type

--- scripts/verifyCommit.cjs
+++ scripts/verifyCommit.cjs
@@ -2,7 +2,19 @@
 const msg = require('fs')
   .readFileSync('.git/COMMIT_EDITMSG', 'utf-8')
   .trim()
-  
+/*
+"build", // 发布版本
+"chore", // 改变构建流程、或者增加依赖库、工具等
+"ci", // 持续集成修改
+"docs", // 文档修改
+"feat", // 新增功能
+"fix",  // 修复缺陷
+"perf", // 优化相关，比如提升性能、体验
+ "refactor", // 代码重构
+ "revert", // 回退版本
+ "style", // 样式修改
+ "test", // 测试用例修改
+*/
 const commitRE = /^(revert: )?(feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release): .{1,50}/
 const mergeRe = /^(Merge pull request|Merge branch)/
 if (!commitRE.test(msg)) {
